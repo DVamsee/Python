89e1fac7c9d7e9bd362599c8bcb1f562c253af69
Vamsee D <dv26460105@gmail.com>
2023-10-25 11:00:32 +0530

    add

--- day1/programs/first program.py
+++ day1/programs/first program.py
@@ -1,6 +1,8 @@
-a = 10
+'''this program will apply arithmeetic perations on two variabel
+this is also a comment'''
+a = 10 # variable a is initialized her
 b =20
-print(a+b)
+print(a+b) #adiition operation
 print(a-b)
 print(a*b)
 print(a/b)
